fa8aabf9dfd43ca5374ed8393353c08dae540159
jennitamontano <31088267+jennitamontano@users.noreply.github.com>
2017-09-13 01:43:24 -0500

    Uso de colores Primary, PrimaryDark y Accent

---
+++ app/src/main/java/mx/jennifermontano/petagrammc/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.jennifermontano.petagrammc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
